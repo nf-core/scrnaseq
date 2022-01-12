bbec4cfa77d5cbc276958335bbeb5b80b5d17590
Alexander Peltzer <alexander.peltzer@boehringer-ingelheim.com>
2022-01-12 15:50:12 +0100

    Add Zenodo DOI

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -9,11 +9,8 @@ class WorkflowMain {
     //
     public static String citation(workflow) {
         return "If you use ${workflow.manifest.name} for your analysis please cite:\n\n" +
-            // TODO nf-core: Add Zenodo DOI for pipeline after first release
-            //"* The pipeline\n" +
-            //"  https://doi.org/10.5281/zenodo.XXXXXXX\n\n" +
             "* The nf-core framework\n" +
-            "  https://doi.org/10.1038/s41587-020-0439-x\n\n" +
+            "  https://doi.org/10.5281/zenodo.4643461\n\n" +
             "* Software dependencies\n" +
             "  https://github.com/${workflow.manifest.name}/blob/master/CITATIONS.md"
     }
